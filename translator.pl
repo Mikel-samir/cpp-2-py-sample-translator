0408ad5074a5516c2a57a8eb0cb115182ccd380d
Michael Samir <michaelsamir1999@gmail.com>
2021-05-27 14:52:21 +0200

    module use fix !

--- translator.pl
+++ translator.pl
@@ -1,8 +1,12 @@
 
-
+:- module(translator,[
+	      py_cond/4,
+	      py_while/3
+	  ]).
 
 :- use_module(library(strings)).
-:- use_module(prelude,string_list_concat/2 as string_concat_list).
+:- use_module(prelude,
+	      [string_list_concat/2 as string_concat_list]).
 
 
 py_cond(T1,Op,T2,O):-
@@ -10,7 +14,7 @@ py_cond(T1,Op,T2,O):-
 
 py_while(Cond,Body,Out):-
     indent_lines("\t",Body,B),
-    string_concat_list(["while (",X,Cond,") : \n",B],Out).
+    string_concat_list(["while (",Cond,") : \n",B],Out).
 
 /** <examples>
   ?- cpp_while("x < 1","x=2\nx=5",O),write(O). 
