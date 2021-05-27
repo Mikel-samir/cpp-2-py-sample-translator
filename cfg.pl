c35e3623f512c589aa6d9d94bb9263c19b54dde7
Michael Samir <michaelsamir1999@gmail.com>
2021-05-27 10:53:48 +0200

    clean to prelude

--- cfg.pl
+++ cfg.pl
@@ -74,64 +74,22 @@ var(S) -->
     %    (nonblank(F1),{char_type(F1,csymf)}),chars(Cs),
     {append([F],Cs,Codes),string_codes(S,Codes)}.
 
-%% variable components not really a char
+%% variable elems parser
 %% it quites if wrong
 velms(S) --> var_elm(S1),velms(S2),
 	     {append([S1],S2,S)}.
 velms(S) --> "",{S=[]}.
-
-
-%% to be generalized
+%% elem
 var_elm(Char,[Char|R],R) :-
      char_type_p(Char,csym,[Char|R],R).
-
-
-
+%% first elem
 var_felm(Char,[Char|R],R) :-
      char_type_p(Char,csymf,[Char|R],R).
 
-%% general char filter
-%% string vs code ??
+%% char type parser
 char_type_p(Char,Type,[Char|R],R):-
     char_type(Char,Type).
 char_type_p([],L,L).
-
-bool_op_p(H,[H|R],R):-
-    member(H,["<=","<"]).
-bool_op_p([],L,L).
-
-
-is_p(H,[H|T]) --> H.
-is_p([],_) --> "".
-
-
-%%% move to a lib file 
-%% slice head and tail of list in reverse order
-
-slice_(H,T,L):-
-    reverse(L,[T|H1]),
-    reverse(H1,H).
-
-revappend(L,[],L).
-revappend(H,T,L):-
-    slice_(H1,T1,L),
-    revappend(H2,T2,H1),
-    H=H2,append_(T1,T2,T).
-
-%% list check
-list([]):-!.
-list(L):-
-    L=[_|_].
-%%safe_append : append any to list
-append_(L1,E,L3):-
-    not(list(E)),list(L1),
-    append(L1,[E],L3),!.
-append_(X,Y,Z):-append(X,Y,Z),!.
-append_(E1,E2,[E1,E2]):-
-    not(list(E1)),not(list(E2)),!.
-append_(E,L2,L3):-
-    not(list(E)),list(L2),
-    append([E],L2,L3),!.
     
     
     
